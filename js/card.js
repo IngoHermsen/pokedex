87b35f3c27cde5ec55d66d0cc79f17aa87f3676b
Ingo Hermsen <ingohermsen@gmail.com>
2022-12-10 23:37:04 +0100

    final version before submit to Developer Akademie

---
+++ js/card.js
@@ -0,0 +1,167 @@
+'use strict'
+
+async function showCard(pokemonID) {
+    let overlayElement = document.getElementById('overlay');
+    let pokemonJson = await getPokemonAsJson(pokemonID);
+
+    renderOverlay(pokemonJson, pokemonID, overlayElement);
+
+    overlayElement.classList.remove('d-none');
+    document.getElementsByTagName('body')[0].classList.add('overflowHidden');
+};
+
+
+function renderOverlay(pokemonJson, pokemonID, overlayElement) {
+    let pokemonArtworkPath = pokemonJson['sprites']['other']['official-artwork']['front_default'];
+    let pokemonName = makeNameInitialUpperCase(pokemonJson['name']);
+    let lastTypeName = pokemonJson['types'][pokemonJson['types'].length - 1]['type']['name'];
+
+    overlayElement.innerHTML = /*html*/ `
+            <div id="card" class="card" onclick="stopPropagation(event)"> 
+                    ${renderCardHead(pokemonArtworkPath, pokemonName, pokemonID, lastTypeName)}
+                    ${renderCardBody(pokemonID)}
+              
+            </div>
+    `
+};
+
+
+function renderCardHead(artworkPath, pokemonName, pokemonID, lastTypeName) {
+    return /*html*/ `
+    <div id=cardHead class="d-flex flex-column ps-3 pt-3 text-light" style="background-color: rgba(${getTypeColor(lastTypeName)})">
+        <i class="closeButton fs-2 fa-regular fa-circle-xmark" onclick="closeOverlay()"></i>
+        <img src="${artworkPath}" alt="officialArtwork">
+        <div class="d-flex align-items-center justify-content-between me-5">
+        <h2 class="fs-1 card-title">${pokemonName}</h2>
+        <h3 class="fs-3">${generateId(pokemonID)}</h3></div>
+    </div>
+    `
+};
+
+
+function renderCardBody(pokemonID) {
+    return /*html*/ `
+    <div class="pt-2 px-5 card-body">
+        <nav class="mb-4 d-flex justify-content-between">
+            <a tabindex="0" id="about" class="text-left stats mx-3" onclick="renderAboutStats(${pokemonID})">About</a>
+            <a tabindex="1" id="baseStats" class="stats mx-3" onclick="renderBaseStats(${pokemonID})">Base Stats</a>
+            <a tabindex="2" id="moves" class="stats mx-3" onclick="renderMovesStats(${pokemonID})">Moves</a>
+        </nav>
+        <div id="cardStatContent" class="d-flex flex-column-reverse align-items-start">
+            ${renderAboutStats(pokemonID)}
+        </div>
+    </div>
+    `
+};
+
+
+function focusAboutStats() {
+    let aboutTextElement = document.getElementById('about');
+    aboutTextElement.focus();
+};
+
+
+function closeOverlay() {
+    document.getElementById('overlay').classList.add('d-none');
+    document.getElementsByTagName('body')[0].classList.remove('overflowHidden');
+};
+
+
+
+async function renderAboutStats(pokemonID) {
+    let pokemonJson = await getPokemonAsJson(pokemonID);
+    let cardStatContentElement = document.getElementById('cardStatContent');
+    cardStatContentElement.innerHTML = /*html*/ `
+    <table id="aboutTable">
+        <tr><td >Types:</td>
+            <td id="typesList">${getTypesHtml(pokemonJson)}</td></tr>
+        <tr><td>Abilities:</td>
+            <td id="abilitiesList">${getAbilitiesHtml(pokemonJson)}</td></tr>
+        <tr><td>Height:</td>
+            <td id="height">${(pokemonJson['height'] / 10).toFixed(2)} m</td></tr>
+        <tr><td>Weight:</td>
+            <td id="weight">${(pokemonJson['weight'] / 10).toFixed(2)} kg</td></tr>
+    </table> `;
+    focusAboutStats();
+};
+
+
+
+function getTypesHtml(pokemonJson) {
+    let typesHtml = "";
+    for (let i = 0; i < pokemonJson['types'].length; i++) {
+        typesHtml += `<span class="properties py-1 px-2">${pokemonJson['types'][i]['type']['name']}</span>`;
+    }
+    return typesHtml;
+
+};
+
+
+function getAbilitiesHtml(pokemonJson) {
+    let abilitiesHtml = "";
+    for (let i = 0; i < pokemonJson['abilities'].length; i++) {
+        abilitiesHtml += `<span class="properties py-1 px-2">${pokemonJson['abilities'][i]['ability']['name']}</span>`;
+    }
+    return abilitiesHtml;
+
+};
+
+
+async function renderBaseStats(pokemonID) {
+    let pokemonJson = await getPokemonAsJson(pokemonID);
+    let cardStatContentElement = document.getElementById('cardStatContent');
+    cardStatContentElement.innerHTML = /*html*/ `
+    <table id="aboutTable">
+        ${renderBaseStatRow('Health: ', pokemonJson, 0, 'success')}
+        ${renderBaseStatRow('Attack: ', pokemonJson, 1, 'warning')}
+        ${renderBaseStatRow('Defense: ', pokemonJson, 2, 'danger')}
+        ${renderBaseStatRow('Special-Attack: ', pokemonJson, 3, 'primary')}
+        ${renderBaseStatRow('Special-Defense: ', pokemonJson, 4, 'info')}
+        ${renderBaseStatRow('Speed: ', pokemonJson, 5, 'secondary')}
+    </table>
+    `;
+};
+
+
+function renderBaseStatRow(statName, pokemonJson, propertyID, bgColor) {
+    return /*html*/ `<tr><td >${statName}</td>
+    <td id="health">${renderProgressBar(pokemonJson, propertyID, bgColor)}</td></tr>
+    `
+};
+
+
+function renderProgressBar(pokemonJson, propertyID, color) {
+    let maxPropertyValue = 255
+    let propertyValue = pokemonJson['stats'][propertyID]['base_stat']
+    let propertyProgressValue = ((100 / maxPropertyValue) * propertyValue)
+
+    let progressBarHtml = /*html*/ `
+    <div class="progress" style="height: 15px">
+        <div class="progress-bar-striped bg-${color} fw-bold text-light ps-2 pb-4" role="progressbar" aria-label="Example 1px high" style="width: ${propertyProgressValue}%;" aria-valuenow="40" aria-valuemin="0" aria-valuemax="255">${propertyValue}</div>
+    </div>
+    `;
+
+    return progressBarHtml;
+};
+
+
+async function renderMovesStats(pokemonID) {
+    let pokemonJson = await getPokemonAsJson(pokemonID);
+    let cardStatContentElement = document.getElementById('cardStatContent');
+
+    cardStatContentElement.innerHTML = /*html*/ `
+    <div class="movesWrapper">
+        ${getMovesHtml(pokemonJson)}
+    </div>
+    `
+    };
+
+
+function getMovesHtml(pokemonJson) {
+    let movesHtml = "";
+    for (let i = 0; i < pokemonJson['moves'].length; i++) {
+        movesHtml += `<div class="properties px-2 py-1 d-inline-block">${pokemonJson['moves'][i]['move']['name']}</div>`
+    };
+    return movesHtml
+
+};
--- js/script.js
+++ js/script.js
@@ -1,1 +1,253 @@
 'use strict'
+
+let offsetValue = 0;
+let renderLimit = 24;
+let amountRendered = 0;
+let totalLimit = 900;
+let indexesOfCurrentSelection = [];
+let activeSearchFilter = false;
+let mainJson;
+let loading = false;
+
+async function loadInitData() {
+    await createPokemonJson(offsetValue, renderLimit);
+    await fillIDArrayFromMainJson();
+    await renderPreviewCards(renderLimit);
+    initSearchOnEnterEvent();
+    initLoadOnScroll();
+};
+
+
+function stopPropagation(event) {
+    event.stopPropagation();
+};
+
+
+function initLoadOnScroll() {
+    window.addEventListener('scroll', async () => {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && !loading) {
+            loading = !loading
+            await renderMorePreviewCards();
+            loading = !loading
+        }
+    });
+};
+
+
+async function createPokemonJson(offsetValue, limitValue) {
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=${limitValue}&offset=${offsetValue}`;
+    let response = await fetch(url);
+    let responseAsJson = await response.json();
+
+    mainJson = responseAsJson;
+};
+
+
+async function fillIDArrayFromMainJson() {
+    indexesOfCurrentSelection = [];
+    for (let i = 0; i < mainJson['results'].length; i++) {
+        let currentPokemonUrl = mainJson['results'][i]['url'];
+        let currentPokemonJson = await getPokemonAsJson(slicePokemonIDfromUrl(currentPokemonUrl));
+        indexesOfCurrentSelection.push(currentPokemonJson['id']);
+    };
+};
+
+
+function slicePokemonIDfromUrl(url) {
+    let urlOfPokemon = url.slice(0, -1); /* slicing the last "Slash" */;
+    let pokemonID = urlOfPokemon.slice(34);
+
+    return pokemonID;
+};
+
+
+async function getPokemonAsJson(pokemonID) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokemonID}`
+    let response = await fetch(url);
+    let responseAsJson = await response.json();
+    let pokemonAsJson = responseAsJson;
+    return pokemonAsJson;
+};
+
+
+async function renderPreviewCards(limit) {
+    amountRendered = 0;
+    let previewCardsElement = document.getElementById('previewCards');
+    let loopLimit = indexesOfCurrentSelection.length < limit ? indexesOfCurrentSelection.length : limit;
+
+    previewCardsElement.innerHTML = '';
+    for (let entry = 0; entry < loopLimit; entry++) {
+        await renderPreviewCard(indexesOfCurrentSelection[entry]);
+    };
+};
+
+
+async function renderPreviewCard(pokemonID) {
+    let pokemonJson = await getPokemonAsJson(pokemonID);
+    let pokemonArtworkPath = pokemonJson['sprites']['other']['official-artwork']['front_default'];
+    let pokemonName = makeNameInitialUpperCase(pokemonJson['name']);
+
+    generatePreviewCardHTML(pokemonArtworkPath, pokemonName, pokemonID, pokemonJson);
+    amountRendered++;
+};
+
+
+function generatePreviewCardHTML(artworkPath, nameString, pokemonID, pokemonJson) {
+    let lastTypeName = pokemonJson['types'][pokemonJson['types'].length - 1]['type']['name'];
+
+    document.getElementById('previewCards').innerHTML += /*html*/`
+    <div id="pokemon${pokemonID}" class="previewCard px-3 pt-3 text-light" style="background-color: rgba(${getTypeColor(lastTypeName)},0.7)"
+            onclick="showCard(${pokemonID})">
+     ${renderArtwork(artworkPath, pokemonID)} <img class="pokeball-bg" src="img/pokeball.png" alt="pokeball">${renderNameAndId(nameString, pokemonID)}    
+    </div>
+    <div class="previewTypes mt-2 fs-5 d-flex flex-column">
+        ${renderTypes(pokemonJson)}
+    </div>
+    `
+};
+
+
+function renderNameAndId(pokemonName, pokemonID) {
+    return /*html*/ `<div class="d-flex align-items-end justify-content-between"><h3>${pokemonName}</h3><h4 class="fs-5">${generateId(pokemonID)}</h4>
+    `
+};
+
+
+function generateId(pokemonID) {
+    return (pokemonID < 10 ? ('#00' + pokemonID) :
+        pokemonID < 100 ? ('#0' + pokemonID) :
+            ('#' + pokemonID));
+};
+
+
+function renderArtwork(artworkPath, pokemonID) {
+    return /*html*/ `<div class="artworkWrapper"><img class="artwork" id="artwork${pokemonID}" src="${artworkPath}" alt="officialPokemonArtwork"></div>`
+};
+
+
+function renderTypes(pokemonJson) {
+    let typesHtml = "";
+    for (let i = 0; i < pokemonJson['types'].length; i++) {
+        let pokemonType = pokemonJson['types'][i]['type']['name'];
+        typesHtml += /*html*/ `<span class="type d-flex justify-content-center mt-2 text-center" style="background-color:rgb(${getTypeColor(pokemonType)}">${pokemonType}</span>`;
+    };
+
+    return typesHtml;
+};
+
+
+function makeNameInitialUpperCase(pokemonName) {
+    return (pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1));
+
+};
+
+
+function getTypeColor(type) {
+    for (let i = 0; i < typeColors.length; i++) {
+        if (typeColors[i]['typeName'] == type) {
+            return typeColors[i]['color'];
+        }
+    };
+};
+
+
+async function initSearchOnEnterEvent() {
+    let searchInputElement = document.getElementById('search');
+    searchInputElement.addEventListener('keyup', e => {
+        if (e.key === 'Enter') { search() };
+    });
+};
+
+
+async function search() {
+    let searchInputElement = document.getElementById('search');
+    if (searchInputElement.value.trim().length == 0) {
+        resetOffsetAndActiveSearchVariables();
+        await createPokemonJson(offsetValue, renderLimit);
+        await fillIDArrayFromMainJson();
+    } else {
+        activeSearchFilter = true;
+        await createPokemonJson(0, 984);
+        indexesOfCurrentSelection = [];
+        fillIDArrayWithMatches(searchInputElement);
+    };
+    renderResultInfoTextElement(searchInputElement);
+    await renderPreviewCards(renderLimit);
+
+};
+
+function resetOffsetAndActiveSearchVariables() {
+    let renderResultInfoTextElement = document.getElementById('resultInfoText')
+    renderResultInfoTextElement.innerHTML = "";
+    offsetValue = 0;
+    activeSearchFilter = false;
+
+};
+
+function renderResultInfoTextElement(searchInputElement) {
+    let renderResultInfoTextElement = document.getElementById('resultInfoText');
+    let indexesLength = indexesOfCurrentSelection.length
+    if (activeSearchFilter) {
+        if (indexesLength == 0) {
+            renderResultInfoTextElement.innerHTML = /*html*/ `
+        Sorry, no matches for "<div>${searchInputElement.value}</div>"`
+        } else {
+            renderResultInfoTextElement.innerHTML = /*html*/ `
+            <div>We found ${indexesLength} ${getMatchFoundText(indexesLength)} for&nbsp</div><div>"${searchInputElement.value}:  "</div>`
+        };
+        searchInputElement.value = '';
+    };
+};
+
+function getMatchFoundText(matchesAmount) {
+    if(matchesAmount == 1) {
+        return 'match';
+    } else {
+        return 'matches';
+    };
+};
+
+
+function fillIDArrayWithMatches(searchInputElement) {
+    for (let i = 0; i < mainJson['results'].length; i++) {
+        if (mainJson['results'][i]['name'].includes(searchInputElement.value.trim().toLowerCase())) {
+            let pokemonID = slicePokemonIDfromUrl(mainJson['results'][i]['url']);
+            indexesOfCurrentSelection.push(pokemonID);
+        };
+    };
+};
+
+
+async function renderMorePreviewCards() {
+    offsetValue = amountRendered;
+    let amountToRender = await setAmountToRender();
+
+    for (let i = offsetValue; i < (offsetValue + amountToRender); i++) {
+        await renderPreviewCard(indexesOfCurrentSelection[i])
+    };
+};
+
+
+async function setAmountToRender() {
+    let amountToRender;
+    if (activeSearchFilter) {
+        if (amountRendered < indexesOfCurrentSelection.length) {
+            amountToRender = compareToRenderLimit(indexesOfCurrentSelection.length);
+        };
+    } else if (amountRendered < totalLimit) {
+        offsetValue = 0;
+        amountToRender = compareToRenderLimit(totalLimit);
+        await createPokemonJson(amountRendered, amountToRender);
+        await fillIDArrayFromMainJson();
+    };
+    return amountToRender;
+};
+
+
+function compareToRenderLimit(endValue) {
+    let differenceValue = (endValue - amountRendered);
+
+    if (differenceValue < renderLimit) {
+        return (differenceValue);
+    } else { return renderLimit };
+};
